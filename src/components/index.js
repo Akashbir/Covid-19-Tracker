01bf7744bc0bea596fd2271ed33e34b92d31bb02
Akashbir Singh <akashbir@Akashbirs-MacBook-Pro.local>
2020-04-30 20:25:30 -0400

    added project files

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+// File to export components to make the code in App.js look less cluttered
+
+export {default as Cards} from './Cards/Cards';
+export {default as Chart} from './Chart/Chart';
+export {default as CountryPicker} from './CountryPicker/CountryPicker';
